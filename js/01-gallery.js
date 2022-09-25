2b7d6a93b445f306d6f19ebea57660cc5a4f8a79
Tauzhnianskyi <d.a.tauzhnianskyi@gmail.com>
2022-09-25 16:25:23 +0300

    Completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const items = createGalleryItem(galleryItems);
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML("beforeend", items);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ description, preview, original }) => {
+      return `
+      <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+      `;
+    })
+    .join("");
+}
+gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+
+  const currentImg = evt.target;
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src="${currentImg.dataset.source}" width="1000"/>
+    </div>
+`);
+
+  instance.show();
+  document.addEventListener("keydown", closeModal);
+
+  function closeModal(evt) {
+    if (evt.which === "27") {
+      instance.close();
+    }
+    instance.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const item = createGalleryItem(galleryItems);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ description, preview, original }) => {
+      return `
+     <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+      `;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", onClick);
+gallery.insertAdjacentHTML("beforeend", item);
+
+function onClick(evt) {
+  evt.preventDefault();
+}
+
+let galleryClick = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
